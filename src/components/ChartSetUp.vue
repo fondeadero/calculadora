bea0dd0ce459629ce7e47020b6caafbd7bd29f23
De la Rocha <alberto.delarocha@uabc.edu.mx>
2020-03-31 15:58:27 -0700

    first commit

---
+++ src/components/ChartSetUp.vue
@@ -0,0 +1,37 @@
+<script>
+    //Importing Bar and mixins class from the vue-chartjs wrapper
+    import { Bar, mixins } from 'vue-chartjs'
+    //Getting the reactiveProp mixin from the mixins module.
+    const { reactiveProp } = mixins
+
+    export default({
+        extends: Bar,
+        mixins: [ reactiveProp ],
+        data () {
+            return {
+                //Chart.js options that control the appearance of the chart
+                options: {
+                    scales: {
+                        xAxes: [{
+                            stacked: true
+                        }],
+                        yAxes: [{
+                            stacked: true
+                        }]
+                    },
+                    legend: {
+                        display: true
+                    },
+                    responsive: true,
+                    maintainAspectRatio: false
+                }
+            }
+        },
+        mounted () {
+            // this.chartData is created in the mixin.
+            // If you want to pass options please create a local options object
+            // this.chartData is created in the mixin and contains all the data needed to build the chart.
+            this.renderChart(this.chartData, this.options)
+        }
+    })
+</script>
---
+++ src/views/CompoundInterest.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "CompoundInterest"
+    }
+</script>
+
+<style scoped>
+
+</style>
